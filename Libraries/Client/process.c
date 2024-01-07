c45cebd285beff1aebccd1c227cf546fa97e3717
Manu Van Hoofstadt <55707265+Zerodays-0@users.noreply.github.com>
2024-01-07 18:38:16 +0100

    update socket using recv and send

--- Libraries/Client/process.c
+++ Libraries/Client/process.c
@@ -4,17 +4,30 @@
 // then send the right password (hopefully)
 void handle(int clientfd, char **passwords) {
     char md5hex[32] = {0};
-    char password[MAX_PASS_LEN+1] = {0};
+    char password[MAX_PASS_LEN + 1] = {0};
 
-    // get hash from server
-    read(clientfd, md5hex, sizeof(md5hex));
+    // get hash from server using recv
+    if (recv(clientfd, md5hex, sizeof(md5hex), 0) < 0) {
+        perror("recv");
+        close(clientfd);
+        return;
+    }
+
     printf("gonna crack: %.*s\n", 32, md5hex);
+
     // crack the hash
-    // found is ok because its either a pointer to password 
-    // which is on the stack here or to "nope" which is a static string
     char *found = crack(md5hex, password, passwords);
-    // reply to server
-    write(clientfd, md5hex, 32);
-    write(clientfd, found, strlen(found));
+
+    // reply to server using send
+    if (send(clientfd, md5hex, sizeof(md5hex), 0) < 0) {
+        perror("send");
+        close(clientfd);
+        return;
+    }
+    
+    if (send(clientfd, found, strlen(found), 0) < 0) {
+        perror("send");
+    }
+
     close(clientfd);
 }
